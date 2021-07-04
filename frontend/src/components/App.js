b835e22bc911cfa6a5017949844474770307590e
Nil Andreu <nilandreug@gmail.com>
2021-07-04 09:21:27 +0200

    Confugration solved bugs

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react-dom'
-import ReactDOM from 'react'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 
-export default class App extends React.Component {
+class App extends Component {
     render() {
         return <h1>React App</h1>
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app')); 
