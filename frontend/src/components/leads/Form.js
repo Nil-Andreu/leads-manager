1632f422e8deee65add85973b35ac34e32ec5634
Nil Andreu <nilandreug@gmail.com>
2021-07-05 11:41:33 +0200

    Basic leads component

---
+++ frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <h1>
+                Add lead form
+            </h1>
+        )
+    }
+}
+
+export default Form
