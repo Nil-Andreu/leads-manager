67c18e70ddba7547ba62c6993710bb20fb0ff15a
Nil Andreu <nilandreug@gmail.com>
2021-07-12 21:52:11 +0200

    Reducer

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,2 +1,2 @@
-// This is only a place to store the types
+// This is only a place to store the types for organization
 export const GET_LEADS = 'GET_LEADS';
--- frontend/src/reducers/leads.js
+++ frontend/src/reducers/leads.js
@@ -1,1 +1,26 @@
 import { GET_LEADS } from '../actions/types'
+
+// Now we create the initial state
+const initialState = {
+    leads: []
+}
+
+export default function(state = initialState, action) {
+    // We have to evaluate the action type
+    switch(action.type) {
+        case GET_LEADS:
+            // In the case that it gets leads, we need to return which will be the state
+            return {
+                // We will spread whathever thing is in the state, which in this case will be leads: []
+                ...state,
+
+                // And we want to fill leads with the ones fetched from the server
+                // which i am going to send those as a paid load in the action
+                leads: action.payload
+            }
+
+        // The case by default
+        default:
+            return state;
+    }
+}
