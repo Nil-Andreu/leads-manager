3ba6879262b1d4290cc87f6c4eecda99f6d208aa
Nil Andreu <65730003+Nil-Andreu@users.noreply.github.com>
2021-07-22 12:44:01 +0200

    update serializers.py

--- leads/serializers.py
+++ leads/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
 from .models import Lead
+from rest_framework import serializers
 
 # We are turning the model into a seralizer
 class LeadSerializer(serializers.ModelSerializer):
